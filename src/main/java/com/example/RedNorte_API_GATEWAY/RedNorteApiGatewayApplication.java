ede4e5cef6d2cec3c2b686393689582eb698c7e5
niloheri123-ux <niloheri123@gmail.com>
2026-05-05 18:45:16 -0400

    api

---
+++ src/main/java/com/example/RedNorte_API_GATEWAY/RedNorteApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.RedNorte_API_GATEWAY;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedNorteApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedNorteApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RedNorte_API_GATEWAY/RedNorteApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RedNorte_API_GATEWAY;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedNorteApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
