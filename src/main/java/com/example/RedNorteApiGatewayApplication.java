1193eb284b8977d94e38a60f309e5de5f06a0e36
AzureTear <ba.maiolatesi@duocuc.cl>
2026-06-14 21:35:25 -0400

    Change dependency

--- src/main/java/com/example/RedNorteApiGatewayApplication.java
+++ src/main/java/com/example/RedNorteApiGatewayApplication.java
@@ -3,11 +3,11 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(excludeName = {
+    "org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration"
+})
 public class RedNorteApiGatewayApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(RedNorteApiGatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(RedNorteApiGatewayApplication.class, args);
+    }
 }
